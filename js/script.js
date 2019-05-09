254e2933d43282eb16ff8ee1fd75f32be4d59731
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-05-09 20:48:27 +0200

    Add Glyphter font

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@
 
 var hamburgerBtn = document.querySelector("#menu-btn");
 var sideBar = document.querySelector("#side-bar");
+var sideBarBrand = document.querySelectorAll(".side-bar-brand")[0];
 var wrapper = document.querySelector("#wrapper");
 
 hamburgerBtn.addEventListener("click", x => {
     hamburgerBtn.classList.toggle("active");
     sideBar.classList.toggle("active");
+    sideBarBrand.classList.toggle("hide");
     wrapper.classList.toggle("active");
 })
