f8eb25cad581434b3be6c00fd880013539904be6
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-05-09 23:36:45 +0200

    Styling navbar - not finished

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 var hamburgerBtn = document.querySelector("#menu-btn");
 var sideBar = document.querySelector("#side-bar");
 var sideBarBrand = document.querySelectorAll(".side-bar-brand")[0];
+var sideBarBtns = document.querySelectorAll(".side-bar-menu-item");
 var wrapper = document.querySelector("#wrapper");
 
 hamburgerBtn.addEventListener("click", x => {
@@ -11,4 +12,7 @@ hamburgerBtn.addEventListener("click", x => {
     sideBar.classList.toggle("active");
     sideBarBrand.classList.toggle("hide");
     wrapper.classList.toggle("active");
+    sideBarBtns.forEach(x => {
+        x.classList.toggle("hide");
+    })
 })
