6479e4955c3af4fefb423e75fc856d8e2f67ddb4
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-05-11 00:23:25 +0200

    Finish side-bar logic, minor styling left to do

--- js/script.js
+++ js/script.js
@@ -4,15 +4,19 @@
 var hamburgerBtn = document.querySelector("#menu-btn");
 var sideBar = document.querySelector("#side-bar");
 var sideBarBrand = document.querySelectorAll(".side-bar-brand")[0];
-var sideBarBtns = document.querySelectorAll(".side-bar-menu-item");
+var sideBarBtns = document.querySelectorAll(".side-bar-menu-slide");
+var sideP = document.querySelectorAll(".p-slide");
 var wrapper = document.querySelector("#wrapper");
 
 hamburgerBtn.addEventListener("click", x => {
     hamburgerBtn.classList.toggle("active");
     sideBar.classList.toggle("active");
     sideBarBrand.classList.toggle("hide");
+    sideP.forEach(x => {
+        x.classList.toggle("hide");
+    });
     wrapper.classList.toggle("active");
     sideBarBtns.forEach(x => {
-        x.classList.toggle("hide");
+        x.classList.toggle("short");
     })
 })
