53e36455ac9bf6bb6e7892fc87c57c053f9a1806
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-05-29 16:48:17 +0200

    Add modals, further modal styling to be done

---
+++ js/modal.js
@@ -0,0 +1,51 @@
+// OPEN & CLOSE functions
+
+function closeModal() {
+  document.querySelectorAll('.modal').forEach(function(x) {
+    x.classList.remove('show');
+  });
+}
+
+function openModal(modalName) {
+  document.querySelector(modalName).classList.add('show');
+}
+
+// CLOSE EACH and every modal with X icon
+document.querySelectorAll('.js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+// ----------------------
+// MODALS: -----------
+// ----------------------
+
+// QUIT modal - LAYOUT nunjucks
+document.querySelector('#open-quit').addEventListener('click', function(e) {
+  e.preventDefault();
+  openModal('#modal-quit');
+});
+
+// LOGIN modal - LAYOUT nunjucks
+document.querySelector('#open-login').addEventListener('click', function(e) {
+  e.preventDefault();
+  openModal('#modal-login');
+});
+
+// URL modal - LINKs nunjucks
+document.querySelectorAll('.open-links').forEach(function(x) {
+  x.addEventListener('click', function(e) {
+    e.preventDefault();
+    openModal('#modal-links');
+  });
+});
+
+// BANNERS modal - BANNER nunjucks
+document.querySelectorAll('.open-banners').forEach(function(x) {
+  x.addEventListener('click', function(e) {
+    e.preventDefault();
+    openModal('#modal-banners');
+  });
+});
